caf122a2c54e14a4a6091a22cacdc794f67568d3
TheQuiBee <caileanqwilliams@gmail.com>
2025-07-22 22:12:55 -0500

    Actually logging?

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -392,7 +392,7 @@ export class PTUActor extends Actor {
       if (key == "hardened" || key == "flinch_count" || key == 'immuneToEffectDamage') continue;
       if (key == "damageBonus" || key == "damageReduction" || key == "evasion") {
         for (let [subkey, value] of Object.entries(mod)) {
-          logger.log("key: " + key + " subkey: " + subkey + " value: " + value)
+          console.log("key: " + key + " subkey: " + subkey + " value: " + value);
           data.modifiers[key][subkey]["total"] = (value["value"] ?? 0) + (value["mod"] ?? 0);
         }
         continue;
@@ -455,6 +455,7 @@ export class PTUActor extends Actor {
       if (key == "hardened" || key == "flinch_count" || key == 'immuneToEffectDamage') continue;
       if (key == "damageBonus" || key == "damageReduction" || key == "evasion") {
         for (let [subkey, value] of Object.entries(mod)) {
+          console.log("key: " + key + " subkey: " + subkey + " value: " + value);
           data.modifiers[key][subkey]["total"] = (value["value"] ?? 0) + (value["mod"] ?? 0);
         }
         continue;
